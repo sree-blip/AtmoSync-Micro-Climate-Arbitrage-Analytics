29ea4635957f04896b149a886abddccbfc3854d7
shivajidesai-bit <shivajidesai4242@gmail.com>
2026-09-02 19:50:30 +0530

    Update day01_snowflake_setup.sql

--- SQL/day01_snowflake_setup.sql
+++ SQL/day01_snowflake_setup.sql
@@ -1,1 +1,24 @@
 
+-- AtmoSync Project
+-- Day 1: Snowflake Setup
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS ATMOSYNC_DB;
+
+-- Use database
+USE DATABASE ATMOSYNC_DB;
+
+-- Create warehouse
+CREATE WAREHOUSE IF NOT EXISTS ATMOSYNC_WH
+WAREHOUSE_SIZE = 'X-SMALL'
+AUTO_SUSPEND = 60
+AUTO_RESUME = TRUE;
+
+-- Use warehouse
+USE WAREHOUSE ATMOSYNC_WH;
+
+-- Verify database
+SHOW DATABASES;
+
+-- Verify warehouse
+SHOW WAREHOUSES;
